0b7faf525b74bbb313ec7272d36e7c15f4fc893f
Janeadaobi <adaobijane6@gmail.com>
2023-05-01 06:21:37 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (success)
+ */
+{
+int main(void)
+int i = 'a';
+int j = 'A';
+while (i <= 'z')
+{
+putchar(i);
+i++;
+}
+while (j <= 'Z');
+{
+putchar(j);
+j++;
+}
+putchar('\n');
+return (0);
+}
